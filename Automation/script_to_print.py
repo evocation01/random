ef4bfcf682cc6091e8a405c98e526c090495d9f4
hakan ispir <hakanispir2004@gmail.com>
2023-12-08 22:08:55 +0300

    Add a main func for script_to_print.py file, add random.toml file for disabling 'pointless-string-statement'

--- Automation/script_to_print.py
+++ Automation/script_to_print.py
@@ -1,6 +1,21 @@
-"""Adding a print function for easier use later"""
+"""Adding a print function for easier use later:"""
 
 
 def print_func(name):
     """code"""
+    if name == r"(\w), (\w)":
+        name == r"g<2> g<1>"
+
     print(f"hello, {name}")
+
+
+"""Main function:"""
+
+
+def main():
+    """Test cases"""
+    print_func("İspir, Hakan")
+
+
+if __name__ == "__main__":
+    main()
