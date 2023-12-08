8717a6b453e909d2f5e51a21a0b62ca58ea60f28
hakan ispir <hakanispir2004@gmail.com>
2023-12-08 21:21:11 +0300

    Add pylint comments to ignore unused-variable warning

--- Learning/Comprehensions.py
+++ Learning/Comprehensions.py
@@ -3,7 +3,9 @@
 
 def never_using_comprehensions():
     """Some comprehension examples."""
+    # pylint: disable=unused-variable
     dict_comp = {i: i * i for i in range(10)}
     list_comp = [x * x for x in range(10)]
     set_comp = {i % 3 for i in range(10)}
     gen_comp = (2 * x + 5 for x in range(10))
+    # pylint: enable=unused-variable
