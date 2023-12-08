2fa6b90a94e2a508abd4b40273984e8a98aa7d91
hakan ispir <hakanispir2004@gmail.com>
2023-12-08 21:09:35 +0300

    rename to comprehensions.py, add docstrings to the outside/inside the func as per pylint guidelines

--- Learning/Comprehensions.py
+++ Learning/Comprehensions.py
@@ -1,4 +1,8 @@
+"""Some examples of never using comprehensions."""
+
+
 def never_using_comprehensions():
+    """Some comprehension examples."""
     dict_comp = {i: i * i for i in range(10)}
     list_comp = [x * x for x in range(10)]
     set_comp = {i % 3 for i in range(10)}
