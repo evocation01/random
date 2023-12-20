59f1ae8ba726d28c6245c06d246232411f6bf35c
hakan ispir <hakanispir2004@gmail.com>
2023-12-20 15:38:18 +0300

    modify meeting_reminder.sh

--- Coursera/Troubleshooting/sending_email_reminders/meeting_reminder.sh
+++ Coursera/Troubleshooting/sending_email_reminders/meeting_reminder.sh
@@ -1,10 +1,12 @@
-!/bin/bash
+#!/bin/bash
 
 meeting_info=$(zenity --forms \
     --title "Meeting" --text "Reminder information" \
     --add-calendar "Date" --add-entry "Title" \
     --add-entry "Emails" \
     2>/dev/null)
+    
+echo $meeting_info
 
 if [[ -n "$meeting_info" ]]; then
     python send_reminders.py "$meeting_info"
