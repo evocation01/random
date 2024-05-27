ebf0d31bc3b256722747143790d2836ae6579d42
hakan ispir <hakanispir2004@gmail.com>
2024-05-27 20:35:19 +0300

    numeric project completed

---
+++ Projects/java/src/main/java/com/example/ManageYourFavouriteFood/testttt.java
@@ -0,0 +1,8 @@
+package main.java.com.example.ManageYourFavouriteFood;
+
+public static void main(String[] args) {
+    int i = 4;
+    for (i = 1; i <= 3; i++) {
+        System.out.println(i);
+    }
+}
